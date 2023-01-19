6ac18afa06cc2c3b368b88f9260a5709071cd24d
Shabdik Chakraborty <112231923+ninja-hattori@users.noreply.github.com>
2023-01-19 10:08:06 +0530

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,141 @@
+let twitter = 'http://www.twitter.com/alvaldes_';
+let linkedin = 'https://www.linkedin.com/in/alvaldes/';
+let github = 'https://github.com/alvaldes/';
+let email = 'mailto:angelluis2605@gmail.com';
+let website = 'https://www.alvaldes.vercel.app/';
+let repo = 'https://github.com/alvaldes/aterm';
+let existCommands = [
+  'about',
+  'banner',
+  'clear',
+  'cls',
+  'date',
+  'education',
+  'email',
+  'github',
+  // 'gui',
+  'help',
+  'history',
+  'linkedin',
+  'projects',
+  'repo',
+  'social',
+  // 'theme',
+  // 'theme ls',
+  // 'theme set',
+  'twitter',
+  'whoami',
+  // 'weather',
+];
+
+about = ["Hello, I'm Angel Valdés👋"];
+
+education = [
+  'Here is my education background!',
+  '<span style="font-weight:bold;">B.Tech in Computer Science</span>'
+]
+
+social = [
+  'twitter           <a href="' +
+  twitter +
+  '" target="_blank">twitter/alvaldes_</a>',
+  'linkedin          <a href="' +
+  linkedin +
+  '" target="_blank">linkedin/alvaldes</a>',
+  'github            <a href="' +
+  github +
+  '" target="_blank">github/alvaldes</a>',
+];
+
+projects = ['On Progress ......'];
+
+// themes = ['dark', 'light'];
+
+// theme = [
+//   'Usage: theme [arg]',
+//   'Args:',
+//   '  - ls: list all available themes',
+//   '  - set: set a theme',
+//   '  - random: set a tandom theme',
+//   '<br>',
+//   'Example:',
+//   '  theme ls # to list all available themes',
+//   '  theme set light # to set a theme',
+// ];
+
+help = [
+  '<span class="command">about</span>              Who is Ninja Hattori?',
+  '<span class="command">banner</span>             Display the header',
+  '<span class="command">clear | cls</span>        Clear the teminal',
+  '<span class="command">date</span>               Display the actual date',
+  '<span class="command">education</span>          my education background',
+  '<span class="command">email</span>              Send me an email',
+  // '<span class="command">gui</span>                Open the Portfolio Website',
+  '<span class="command">help</span>               Check available commands',
+  '<span class="command">history</span>            View command history',
+  '<span class="command">projects</span>           View coding projects',
+  '<span class="command">repo</span>               Open the source code',
+  '<span class="command">social</span>             Display social networks',
+  '<span class="command">whoami</span>             About current user',
+  // '<span class="command">theme</span>              Theme configurations',
+  // '<span class="command">weather</span>            Display the weather of a [city]',
+  '<br>',
+  '[TAB]              Trigger completion',
+  '[Ctrl+l]           Clear terminal',
+  '[Ctrl+p]           Go back to previous command',
+];
+
+banner = [
+'<span class="command">     .........                          </span>',
+'<span class="command">              .::::-:                   </span>',
+'<span class="command">    ........:::::::-+                   </span>',
+'<span class="command">  .         .:::::--:=.    .            </span>',
+'<span class="command">       .....         =#*+++=-...              _  __   ____   _  __     __   ___ </span>',
+'<span class="command">     ..            :*=.    .-=+*=.           / |/ /  /  _/  / |/ / __ / /  / _ |</span>',
+'<span class="command">                   *-          .*#:         /    /  _/ /   /    / / // /  / __ |</span>',
+'<span class="command">                   #-             =:       /_/|_/  /___/  /_/|_/  \\___/  /_/ |_|</span>',
+'<span class="command">                   :*-.                                                         </span>',
+'<span class="command">                     -=+====-::.              __ __   ___  ______ ______  ____    ___    ____</span>',
+'<span class="command">                            .-+#+-=-         / // /  / _ |/_  __//_  __/ / __ \\  / _ \\  /  _/</span>',
+'<span class="command">                                =+..-:      / _  /  / __ | / /    / /   / /_/ / / , _/ _/ /  </span>',
+'<span class="command">                                  +- ..    /_//_/  /_/ |_|/_/    /_/    \\____/ /_/|_| /___/  </span>',
+'<span class="command">                                   =:   </span>',
+'<span class="command">                                    =   </span>',
+'<span class="command">                                    .   </span>',
+'<span class="command">                                    .   </span>',
+'<span class="color2">Hello and welcome to my interactive web terminal!</span>',
+'<span class="color2">If you would like to see a list of the commands that are available, please type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+];
+
+// banner = [
+// '<span class="command">     .........                          </span>',
+// '<span class="command">              .::::-:                   </span>',
+// '<span class="command">    ........:::::::-+                   </span>',
+// '<span class="command">  .         .:::::--:=.    .            </span>',
+// '<span class="command">       .....         =#*+++=-...        </span>',
+// '<span class="command">     ..            :*=.    .-=+*=.      </span>',
+// '<span class="command">                   *-          .*#:     </span>',
+// '<span class="command">                   #-             =:    </span>',
+// '<span class="command">                   :*-.                 </span>',
+// '<span class="command">                     -=+====-::.        </span>',
+// '<span class="command">                            .-+#+-=-    </span>',
+// '<span class="command">                                =+..-:  </span>',
+// '<span class="command">                                  +- .. </span>',
+// '<span class="command">                                   =:   </span>',
+// '<span class="command">                                    =   </span>',
+// '<span class="command">                                    .   </span>',
+// '<span class="command">                                    .   </span>',
+// '<span class="color2">Hello and welcome to my interactive web terminal!</span>',
+// '<span class="color2">If you would like to see a list of the commands that are available, please type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+// ];
+
+
+
+// banner = [
+//   '<div>',
+//   '<p id="welcome"><b>WELCOME</b></p>',
+//   '<img src="images/waving-emon_hi2.gif" class="img1" alt=""><br/>',
+//   '<span class="color2">Hello and welcome to my interactive web terminal!</span>',
+//   '<span class="color2">If you would like to see a list of the commands that are available, please type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+//   '</div>',
+// ];
